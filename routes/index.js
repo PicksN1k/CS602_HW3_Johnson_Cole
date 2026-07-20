74a71aaa98767b49a8e7b70e2cd1489c7909dfe3
PicksN1k <csjohnson2838@gmail.com>
2026-07-20 19:24:56 -0400

    modified:   routes/index.js 	modified:   testCourseModule.js

--- routes/index.js
+++ routes/index.js
@@ -1,111 +1,42 @@
-import express from 'express';
+router.get('/random', async function(req, res) {
 
-const router = express.Router();
+  let result = await courseDB.getRandomCourse();
 
-// Use the course module
-import * as courseDB from '../courseModule.js';
+  res.render('randomView', result);
 
-// router specs
-
-router.use(function (req, res, next){
-  if (req.session.sessionData === undefined) {
-    req.session.sessionData = {
-      'lookupByCourseId': [],
-      'lookupByCourseName': [],
-      'lookupByCoordinator': [],
-      'courseDescrition': []
-    };
-  }
-  next();
 });
 
-// GET request to the homepage
+router.get('/describe/:id', async function(req, res) {
 
-router.get('/', function (req, res){
-  res.render('homeView');
-});
-
-
-router.get('/cid', async function(req, res) {
-  if (req.query.id) {
-    let id = req.query.id;
-    let result = await courseDB.lookupByCourseId(id);
-
-    if (!req.session.sessionData['lookupByCourseId'].includes(encodeURIComponent(id)))
-      req.session.sessionData['lookupByCourseId'].push(encodeURIComponent(id));
-
-    res.render('lookupByCourseIdView', 
-      {query: id, courses: result});
-  } else {
-    res.render('lookupByCourseIdForm');
-  }
-});
-
-router.post('/cid', async function(req, res) {
-  let id = req.body.id;
-  let result = await courseDB.lookupByCourseId(id);
-
-  if (!req.session.sessionData['lookupByCourseId'].includes(encodeURIComponent(id)))
-      req.session.sessionData['lookupByCourseId'].push(encodeURIComponent(id));
-
-    
-  res.render('lookupByCourseIdView', 
-    {query: id, courses: result});
-});
-
-
-router.get('/cid/:id', async function(req, res) {
   let id = req.params.id;
-  
-  // Fill in the code
+  let result = await courseDB.getCourseDescription(id);
+
+  if (!req.session.sessionData['courseDescrition'].includes(id))
+      req.session.sessionData['courseDescrition'].push(id);
+
+  res.render('descriptionView', result);
 
 });
 
 
-router.get('/cname', async function(req, res) {
-  
-  // Fill in the code
+router.get('/coordinator/:id', async function(req, res) {
 
-});
+  let id = req.params.id;
+  let result = await courseDB.lookupByCoordinator(id);
 
-router.post('/cname', async function(req, res) {
-  
-  // Fill in the code
+  if (!req.session.sessionData['lookupByCoordinator'].includes(id))
+      req.session.sessionData['lookupByCoordinator'].push(id);
 
-});
-
-router.get('/cname/:name', async function(req, res) {
-  
-  // Fill in the code
-
-});
-
-router.get('/random', async function (req, res) {
-  
-  // Fill in the code
-  
-});
-
-router.get('/describe/:id', async function (req, res) {
-  
-  // Fill in the code
-
-});
-
-
-router.get('/coordinator/:id', async function (req, res) {
-  
-  // Fill in the code
+  res.render('lookupByCoordinatorView', result);
 
 });
   
-router.get('/history', function (req, res) {
+router.get('/history', function(req, res) {
 
-  // Fill in the code
-  
+  res.render('historyView', {
+    history: req.session.sessionData
+  });
+
 });
-
-
-
 
 export {router};
--- testCourseModule.js
+++ testCourseModule.js
@@ -19,3 +19,25 @@ console.log(result);
 
 // Write One additional test case for each of the above.
 // Make sure lookupByCourseId and lookupByCourseName provide more than one match.
+
+// Additional Test Cases
+
+// More than one match
+result = await courseDB.lookupByCourseId("CS 6%");
+console.log(result);
+
+// More than one match
+result = await courseDB.lookupByCourseName("%Data%");
+console.log(result);
+
+// Different coordinator
+result = await courseDB.lookupByCoordinator('epinsky');
+console.log(result);
+
+// Another random course
+result = await courseDB.getRandomCourse();
+console.log(result);
+
+// Different course description
+result = await courseDB.getCourseDescription('602');
+console.log(result);
