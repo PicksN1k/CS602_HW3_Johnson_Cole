2aba116eaa5cfab89f802cee986faaae8dea3bce
PicksN1k <csjohnson2838@gmail.com>
2026-07-20 18:03:42 -0400

    modified:   models/OneToManyModel.js

--- models/OneToManyModel.js
+++ models/OneToManyModel.js
@@ -2,6 +2,8 @@ import { Course }  from './Course.js';
 import { Coordinator  }  from './Coordinator.js';
 
 // Set the relationships
+Course.belongsTo(Coordinator);
 
+Coordinator.hasMany(Course);
 
 export { Course, Coordinator  };
