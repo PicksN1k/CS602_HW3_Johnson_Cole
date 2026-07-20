c29de690eb21270c57c696371e31a6c1428ca213
PicksN1k <csjohnson2838@gmail.com>
2026-07-20 18:01:31 -0400

    modified:   models/Coordinator.js

--- models/Coordinator.js
+++ models/Coordinator.js
@@ -1,10 +1,28 @@
 import { DataTypes } from 'sequelize';
-import { db }        from '../db.js';
+import { db } from '../db.js';
 
 const Coordinator = db.define(
   'Coordinator',
   {
-    // Fill in the code
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true
+    },
+
+    firstName: {
+      type: DataTypes.STRING
+    },
+
+    lastName: {
+      type: DataTypes.STRING
+    },
+
+    fullName: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return `${this.firstName} ${this.lastName}`;
+      }
+    }
   }
 );
 
