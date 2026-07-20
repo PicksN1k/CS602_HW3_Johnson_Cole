6136ae8100ee1ba2de233ee3f91dc10f29ef369e
PicksN1k <csjohnson2838@gmail.com>
2026-07-20 19:37:20 -0400

    modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,117 @@
-router.get('/random', async function(req, res) {
+import express from 'express';
 
-  let result = await courseDB.getRandomCourse();
+const router = express.Router();
 
-  res.render('randomView', result);
+// Use the course module
+import * as courseDB from '../courseModule.js';
+
+// router specs
+
+router.use(function (req, res, next){
+  if (req.session.sessionData === undefined) {
+    req.session.sessionData = {
+      'lookupByCourseId': [],
+      'lookupByCourseName': [],
+      'lookupByCoordinator': [],
+      'courseDescrition': []
+    };
+  }
+  next();
+});
+
+// GET request to the homepage
+
+router.get('/', function (req, res){
+  res.render('homeView');
+});
+
+
+router.get('/cid', async function(req, res) {
+  if (req.query.id) {
+    let id = req.query.id;
+    let result = await courseDB.lookupByCourseId(id);
+
+    if (!req.session.sessionData['lookupByCourseId'].includes(encodeURIComponent(id)))
+      req.session.sessionData['lookupByCourseId'].push(encodeURIComponent(id));
+
+    res.render('lookupByCourseIdView', 
+      {query: id, courses: result});
+  } else {
+    res.render('lookupByCourseIdForm');
+  }
+});
+
+router.post('/cid', async function(req, res) {
+  let id = req.body.id;
+  let result = await courseDB.lookupByCourseId(id);
+
+  if (!req.session.sessionData['lookupByCourseId'].includes(encodeURIComponent(id)))
+      req.session.sessionData['lookupByCourseId'].push(encodeURIComponent(id));
+
+    
+  res.render('lookupByCourseIdView', 
+    {query: id, courses: result});
+});
+
+
+router.get('/cid/:id', async function(req, res) {
+  let id = req.params.id;
+  
+  // Fill in the code
+  let result = await courseDB.lookupByCourseId(id);
+  if (!req.session.sessionData['lookupByCourseId'].includes(encodeURIComponent(id)))
+    req.session.sessionData['lookupByCourseId'].push(encodeURIComponent(id));
+  res.render('lookupByCourseIdView', {
+    query: id,
+    courses: result
+  });
+});
+
+
+router.get('/cname', async function(req, res) {
+  
+  // Fill in the code
 
 });
 
-router.get('/describe/:id', async function(req, res) {
+router.post('/cname', async function(req, res) {
+  
+  // Fill in the code
 
-  let id = req.params.id;
-  let result = await courseDB.getCourseDescription(id);
+});
 
-  if (!req.session.sessionData['courseDescrition'].includes(id))
-      req.session.sessionData['courseDescrition'].push(id);
+router.get('/cname/:name', async function(req, res) {
+  
+  // Fill in the code
 
-  res.render('descriptionView', result);
+});
+
+router.get('/random', async function (req, res) {
+  
+  // Fill in the code
+  
+});
+
+router.get('/describe/:id', async function (req, res) {
+  
+  // Fill in the code
 
 });
 
 
-router.get('/coordinator/:id', async function(req, res) {
-
-  let id = req.params.id;
-  let result = await courseDB.lookupByCoordinator(id);
-
-  if (!req.session.sessionData['lookupByCoordinator'].includes(id))
-      req.session.sessionData['lookupByCoordinator'].push(id);
-
-  res.render('lookupByCoordinatorView', result);
+router.get('/coordinator/:id', async function (req, res) {
+  
+  // Fill in the code
 
 });
   
-router.get('/history', function(req, res) {
+router.get('/history', function (req, res) {
 
-  res.render('historyView', {
-    history: req.session.sessionData
-  });
-
+  // Fill in the code
+  
 });
+
+
+
 
 export {router};
